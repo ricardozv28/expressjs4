3ca65d41d446e046acd566ee24fe0fdaa7a8e3c7
Willam Zavala <ricardozv_28@hotmail.com>
2016-11-03 20:28:57 -0600

    startup basic functionality

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+var ejs = require('ejs');
 var app = express();
 
-
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/home.html');
+ response.sendFile(__dirname + '/home.html');
+})
+app.get('/courses', function(request, response) {
+ fs.readFile('course.json', 'utf8', function(err, data) {
+ var courses = JSON.parse(data).courses;
+ response.locals = { courses: courses };
+ response.render('courses.ejs');
+ });
 });
-
 app.listen(8000);
 console.log('Server running http://localhost:8000');
